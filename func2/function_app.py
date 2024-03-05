55fb12c6e6248838a8fab1c1d88e3a45ac502270
bhanu-kanamarlapudi <bhanukaushik2310@gmail.com>
2024-03-05 15:04:29 -0500

    modified both functions

--- func2/function_app.py
+++ func2/function_app.py
@@ -20,6 +20,6 @@ def http_trigger_func2(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function-2 executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function-2 executed successfully",
              status_code=200
         )
